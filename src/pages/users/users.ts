cddfbd5796e05e1783e8e4ed2ed1e599680a4bd0
lakshyajit165 <lakshyajit165@gmail.com>
2018-12-01 19:52:21 +0530

    Committing before first assignment

---
+++ src/pages/users/users.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UserPage } from './user/user';
+
+/**
+ * Generated class for the UsersPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html',
+})
+export class UsersPage {
+
+  constructor(private navCtrl: NavController){
+
+  }
+
+  onLoadUser(name: string){
+    this.navCtrl.push(UserPage, {userName: name});
+  }
+
+}
